bc9afe38995d56c3ca673a6290379b3f0dbf0f1d
fcurrk <48197714+fcurrk@users.noreply.github.com>
2024-07-19 11:58:08 +0800

    Update

--- app/lib/pages/tabs/settings_tab.dart
+++ app/lib/pages/tabs/settings_tab.dart
@@ -414,47 +414,47 @@ class SettingsTab extends StatelessWidget {
                 ),
               ],
             ),
-            _SettingsSection(
-              title: t.settingsTab.other.title,
-              padding: const EdgeInsets.only(bottom: 0),
-              children: [
-                _ButtonEntry(
-                  label: t.aboutPage.title,
-                  buttonLabel: t.general.open,
-                  onTap: () async {
-                    await context.push(() => const AboutPage());
-                  },
-                ),
-                _ButtonEntry(
-                  label: t.settingsTab.other.support,
-                  buttonLabel: t.settingsTab.other.donate,
-                  onTap: () async {
-                    await context.push(() => const DonationPage());
-                  },
-                ),
-                _ButtonEntry(
-                  label: t.settingsTab.other.privacyPolicy,
-                  buttonLabel: t.general.open,
-                  onTap: () async {
-                    await launchUrl(
-                      Uri.parse('https://localsend.org/privacy'),
-                      mode: LaunchMode.externalApplication,
-                    );
-                  },
-                ),
-                if (checkPlatform([TargetPlatform.iOS, TargetPlatform.macOS]))
-                  _ButtonEntry(
-                    label: t.settingsTab.other.termsOfUse,
-                    buttonLabel: t.general.open,
-                    onTap: () async {
-                      await launchUrl(
-                        Uri.parse('https://www.apple.com/legal/internet-services/itunes/dev/stdeula/'),
-                        mode: LaunchMode.externalApplication,
-                      );
-                    },
-                  ),
-              ],
-            ),
+//            _SettingsSection(
+//              title: t.settingsTab.other.title,
+//              padding: const EdgeInsets.only(bottom: 0),
+//              children: [
+//                _ButtonEntry(
+//                  label: t.aboutPage.title,
+//                  buttonLabel: t.general.open,
+//                  onTap: () async {
+//                    await context.push(() => const AboutPage());
+//                  },
+//                ),
+//                _ButtonEntry(
+//                  label: t.settingsTab.other.support,
+//                  buttonLabel: t.settingsTab.other.donate,
+//                  onTap: () async {
+//                    await context.push(() => const DonationPage());
+//                  },
+//                ),
+//                _ButtonEntry(
+//                  label: t.settingsTab.other.privacyPolicy,
+//                  buttonLabel: t.general.open,
+//                  onTap: () async {
+//                    await launchUrl(
+//                      Uri.parse('https://localsend.org/privacy'),
+//                      mode: LaunchMode.externalApplication,
+//                    );
+//                  },
+//                ),
+//                if (checkPlatform([TargetPlatform.iOS, TargetPlatform.macOS]))
+//                  _ButtonEntry(
+//                    label: t.settingsTab.other.termsOfUse,
+//                    buttonLabel: t.general.open,
+//                    onTap: () async {
+//                      await launchUrl(
+//                        Uri.parse('https://www.apple.com/legal/internet-services/itunes/dev/stdeula/'),
+//                        mode: LaunchMode.externalApplication,
+//                      );
+//                    },
+//                  ),
+//              ],
+//            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
@@ -481,18 +481,18 @@ class SettingsTab extends StatelessWidget {
               '© ${DateTime.now().year} Tien Do Nam',
               textAlign: TextAlign.center,
             ),
-            Center(
-              child: TextButton.icon(
-                style: TextButton.styleFrom(
-                  foregroundColor: Theme.of(context).colorScheme.onSurface,
-                ),
-                onPressed: () async {
-                  await context.push(() => const ChangelogPage());
-                },
-                icon: const Icon(Icons.history),
-                label: Text(t.changelogPage.title),
-              ),
-            ),
+//            Center(
+//              child: TextButton.icon(
+//                style: TextButton.styleFrom(
+//                  foregroundColor: Theme.of(context).colorScheme.onSurface,
+//                ),
+//                onPressed: () async {
+//                  await context.push(() => const ChangelogPage());
+//                },
+//                icon: const Icon(Icons.history),
+//                label: Text(t.changelogPage.title),
+//              ),
+//            ),
             const SizedBox(height: 80),
           ],
         );
